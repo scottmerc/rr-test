86282dcc2a9ec54aa625a52736b59d67069dbbde
Scott Mercer <scottmercer@Scotts-MacBook-Pro-2.local>
2023-04-18 17:46:49 -0700

    updating shell

--- Discovery/start.sh
+++ Discovery/start.sh
@@ -2,4 +2,4 @@
 export PYENV_ROOT="$HOME/.pyenv"
 
 pip3 install -r requirements.txt
-HS_API_TOKEN=04015c97362a4458847cce0a9c1f1fe4 python3 -m pytest -s --disable-warnings
+HS_API_TOKEN=03dcc445a81c48a4a3731563cbf9dd78 HOSTNAME=dev-ca-tor-2-proxy-0-mac.headspin.io python3 -m pytest -s --disable-warnings
